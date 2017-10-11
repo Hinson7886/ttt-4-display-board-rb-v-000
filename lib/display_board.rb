102ea21003365b5545a63bfc7fb47c8903f02e21
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-11 15:44:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,8 +7,8 @@ def display_board(board)
 
   puts "#{board[0]}|#{board[1]}|#{board[2]}   "
   puts "-----------"
-  puts "   |#{board[4]}|   "
+  puts "#{}   |#{board[4]}| #{}  "
   puts "-----------"
-  puts "   |   |#{board[8]}   "
+  puts " #{}  | #{}  |#{board[8]}   "
 end
 display_board(board)
