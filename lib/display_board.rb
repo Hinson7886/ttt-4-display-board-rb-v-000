40989bed37b87bb21f6e574f484e03b52e4d85f7
Jon Hinson <jonhinsonfx@gmail.com>
2017-10-11 14:22:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,7 +2,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 board = ["O "," "," "," "," X"," "," "," "," "]
-board[0] = "O"
+
 def display_board(board)
 
   puts "   |   |   "
@@ -11,4 +11,4 @@ def display_board(board)
   puts "-----------"
   puts "   |   |   "
 end
-display_board(board)
+display_board(board[0])
